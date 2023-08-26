eb568b43f5cea9454662d9ae77d34df2a7d28425
Titovictoor <Titovictoortfm@gmail.com>
2023-08-25 21:09:06 -0300

    Fixbugs

--- recover.js
+++ recover.js
@@ -3,6 +3,6 @@ const submit = document.querySelector(".form-recover");
 submit.addEventListener("submit", (e) => {
     e.preventDefault();
     alert("Check your email");
-    window.location.href = "/";
+    window.location.href = "/Login";
 
 })
